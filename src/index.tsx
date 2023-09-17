c9083135e23a4993b2b71f6a01297144bf535843
Sina Salami <s.salami78@gmail.com>
2023-09-17 16:11:36 +0330

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,23 @@
+import RNAdvertisingId from 'react-native-advertising-id';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export const init = (appToken: string) => {
+  let advertisingId;
+  RNAdvertisingId.getAdvertisingId().then(({ adId }: { adId: string }) => {
+    advertisingId = adId;
+    fetch('https://apptimize-event-staging.yektanet.com/open/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        user_token: advertisingId,
+        app_id: appToken,
+      }),
+    });
+  });
+};
